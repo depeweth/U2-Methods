ae99de5b56ecbe6544f6f182fe0d79b618d69a7f
depeweth000 <depeweth000@warren.k12.in.us>
2021-09-27 12:28:49 -0400

    U2 notes

--- L5-Swing Frames and Buttons/src/FeetToMeters.java
+++ L5-Swing Frames and Buttons/src/FeetToMeters.java
@@ -1,6 +1,69 @@
+import jdk.nashorn.internal.scripts.JO;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class FeetToMeters {
 
     /* Create a program with two buttons. Clicking on the first button will prompt the user to enter a distance in feet and
     convert it to meters. Clicking on the second button will prompt the user to enter a distance in meters and convert to feet.
      */
+
+    /*
+find what meters to feet and feet to meters is
+get buttons setted up
+get them math
+boom bananas
+     */
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("This Is A Simple Window");
+        JPanel panel = new JPanel();
+        JButton feettometers = new JButton("Quote 1");
+        JButton meterstofeet = new JButton("Quote 2");
+
+
+        window.setSize(200, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //window centered on screen
+        window.setLocationRelativeTo(null);
+
+
+        panel.add(feettometers);
+        panel.add(meterstofeet);
+
+        window.add(panel);
+
+
+        feettometers.addActionListener(new FeetToMetersListener());
+        meterstofeet.addActionListener(new MetersToFeetListener());
+
+
+        window.setVisible(true);
+    }
+
+    private static class FeetToMetersListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+
+            double number = Double.parseDouble(JOptionPane.showInputDialog("Enter the Feet;"));
+
+            double results = number / 3.28;
+
+            JOptionPane.showMessageDialog(null, results + " in meters. ");
+        }
+    }
+
+    private static class MetersToFeetListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+
+            double number = Double.parseDouble(JOptionPane.showInputDialog("Enter the Meters;"));
+
+            double results = number / 3.28;
+
+            JOptionPane.showMessageDialog(null, results + " in feet. ");
+        }
+    }
 }
--- L5-Swing Frames and Buttons/src/Quotes.java
+++ L5-Swing Frames and Buttons/src/Quotes.java
@@ -1,8 +1,62 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Quotes {
 
     /*Create a GUI program with three buttons.
     Each button displays one of your favorite quotes.
     Hint: Use \n to make a new line.
      */
+    public static void main(String[] args)
+        {
+            JFrame window = new JFrame("This Is A Simple Window");
+            JPanel panel = new JPanel();
+            JButton button = new JButton("Quote 1");
+            JButton button2 = new JButton("Quote 2");
+            JButton button3 = new JButton("Quote 3");
 
-}
+            window.setSize(800, 600);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //window centered on screen
+            window.setLocationRelativeTo(null);
+
+            button.addActionListener(new ButtonListener1());
+            button2.addActionListener(new ButtonListener2());
+            button3.addActionListener(new ButtonListener3());
+
+            panel.add(button);
+            panel.add(button2);
+            panel.add(button3);
+            window.add(panel);
+
+
+
+            window.setVisible(true);
+
+        }
+
+        private static class ButtonListener1 implements ActionListener {
+
+            public void actionPerformed(ActionEvent e){
+                JOptionPane.showMessageDialog(null, ("no hear"));
+            }
+
+        }
+        private static class ButtonListener2 implements ActionListener{
+            public void actionPerformed(ActionEvent e){
+                JOptionPane.showMessageDialog(null, "no see");
+            }
+
+        }
+        private static class ButtonListener3 implements ActionListener{
+            public void actionPerformed(ActionEvent e){
+                JOptionPane.showMessageDialog(null, "no say");
+            }
+
+        }
+
+
+    }
+
